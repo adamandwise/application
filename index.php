23623ff00a2be2e01fbf593ef0c95dfc83930384
Adam Wise <wise.adam@student.greenriver.edu>
2023-01-20 15:42:08 -0500

    set up mvc, added composer.json, index page, home html page, added some place holder text, just about ready to go

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+
+// turn on reporting errors
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+
+//require the autoload file
+require_once('vendor/autoload.php');
+
+//instantiate the f3 Base Class( or the fat-free object)
+$f3 = Base::instance();
+
+//define a default route(328/application) with anonymous function
+$f3 -> route('GET /', function(){
+    //instantiate a view
+    $view = new Template();
+
+    echo $view -> render('views/home.html');
+});
+
+$f3->run();
+
+
+
+?>
