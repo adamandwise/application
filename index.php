213c5e25d68f22fcbae8b456316a89d109007d82
Adam Wise <wise.adam@student.greenriver.edu>
2023-01-24 17:26:07 -0500

    fix

--- index.php
+++ index.php
@@ -25,6 +25,13 @@ $f3 -> route('GET /', function(){
     echo $view -> render('views/home.html');
 });
 
+$f3 -> route('GET /app', function(){
+    //instantiate a view
+    $view = new Template();
+
+    echo $view -> render('views/application.php');
+});
+
 //run fat free
 $f3->run();
 
