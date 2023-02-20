2a55ad05d7507e90eb6d8967832e2fa5cfb30886
Adam Wise <wise.adam@student.greenriver.edu>
2023-02-19 21:19:01 -0500

    data layer and validation

---
+++ model/data-layer.php
@@ -0,0 +1,23 @@
+<?php
+
+    function getMail()
+    {
+
+
+        return array("JavaScript","PHP","Java","Python","HTML","CSS","ReactJs", "NodeJs");
+    }
+
+    function getVert()
+    {
+        return array("SaaS","Health Tech","Ag Tech","HR Tech","Industrial Tech","Cyber Security");
+    }
+
+    function getExp()
+    {
+        return array("0-2","2-4","4+");
+    }
+
+    function getRelocate()
+    {
+        return array("Yes","No","Maybe");
+    }
---
+++ model/validation.php
@@ -0,0 +1,96 @@
+<?php
+
+    // checks to validate the first name of the user
+    function validFirstName($fname)
+    {
+        $pattern = '/^[a-zA-Z]+$/';
+        return preg_match($pattern,$fname);
+        //return strlen($fname) > 2;
+
+    }
+
+    //checks to validate the last name of the user
+    function validLastName($lname)
+    {
+        $pattern = '/^[a-zA-Z]+$/';
+        return preg_match($pattern,$lname);
+    }
+
+    //checks to validate a github link
+    function validGitHub($github)
+    {
+        if(strpos($github,"github.com/") !== false)
+            return true;
+
+        return false;
+    }
+
+    function validExp($exp)
+    {
+        if(in_array($exp,getExp())){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    function validRelocate($relocate)
+    {
+        if(in_array($relocate,getRelocate())){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    //check to validate the users bio
+    function validBio($bio)
+    {
+        return strlen($bio) > 25;
+    }
+
+    // checks to validate the users cell phone number to a specific format
+    function validPhone($phone)
+    {
+        $pattern = '/^\d{3}-\d{3}-\d{4}$/';
+        if(preg_match($pattern,$phone))
+            return true;
+
+        return false;
+    }
+
+    // checks to validate the users email adress
+    function validEmail($email)
+    {
+        if(strpos($email,"@")!== false)
+            return true;
+
+        return false;
+    }
+
+    //checks to validate that the values associated with the mail array are correct
+    function validSelectionsJobs($mail)
+    {
+
+        foreach($mail as $mail_item){
+                if(!in_array($mail_item,getMail())){
+                    return false;
+                }else{
+                    return true;
+                }
+            }
+    }
+
+    //checks to validate that the values assocaited with the mail array are correct
+    function validSelectionsVerticals($vertical)
+    {
+        foreach($vertical as $vertical_item){
+            if(!in_array($vertical_item,getVert())){
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
+        return true;
+    }
